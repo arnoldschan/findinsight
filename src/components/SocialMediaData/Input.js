f64263028c798c1e64fe75a4cfea761a272cd631
Arnold <arnold.chan@lndata.com>
2020-10-28 09:39:49 +0800

    fix: :lipstick: change to Material UI input style

--- src/components/SocialMediaData/Input.js
+++ src/components/SocialMediaData/Input.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import Typography from "@material-ui/core/Typography";
-import { Box, InputAdornment, makeStyles } from '@material-ui/core';
+import { Input as InputM, Box, Button, FormHelperText, FormLabel, InputAdornment, InputLabel, makeStyles } from '@material-ui/core';
 import Checkbox from "@material-ui/core/Checkbox";
 import FormControl from "@material-ui/core/FormControl";
 import FormGroup from "@material-ui/core/FormGroup";
@@ -19,10 +19,19 @@ const useStyles = makeStyles((theme) => ({
     label: {
         color: theme.palette.text.primary
     },
-    options:{
+    platform:{
         display: 'flex',
         flexDirection: 'row',
         justifyContent: 'center',
+    },
+    options:{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        marginLeft: 20,
+        '& label':{
+            marginBottom: 0,
+        }
     }
     
 }));
@@ -49,45 +58,70 @@ function Input({keyword, setKeyword}) {
             setError(false)
         }
     }
-
+    const [platform, setPlatform] = useState({ig: false, soon1: false, soon2: false})
+    const platformChange = (event) => {
+        setPlatform({ ...platform, [event.target.name]: event.target.checked });
+        console.log(platform)
+    };
     return (
-            <FormControl>
-                <FormGroup className={classes.options}>
+        <React.Fragment>
+        <FormGroup className={classes.platform}>
                     <FormControlLabel
                         className={classes.label}
-                        control={ <Checkbox name="IG" />}
+                        control={ <Checkbox checked={platform.ig} onChange={platformChange} name="ig" />}
                         label="Instagram"
                     />
                     <FormControlLabel
                         className={classes.label}
-                        control={ <Checkbox name="SOON1" />}
+                        control={ <Checkbox checked={platform.soon1} onChange={platformChange} name="soon1" />}
                         label="SOON"
                     />
                     <FormControlLabel
                         className={classes.label}
-                        control={ <Checkbox name="SOON2" />}
+                        control={ <Checkbox checked={platform.soon2} onChange={platformChange} name="soon2" />}
                         label="SOON"
                     />
                 </FormGroup>
             <Box display='flex' flexDirection="row" alignItems="center" >
-                <Typography className={classes?.root}>Keyword:</Typography>
-                    <InputBox 
-                    onChange={e=>keywordValidate(e)}
-                    value={keyword}
-                    error={error}
-                    multiline
-                    helperText="press 'return' for multiple keyword search"
-                    label="Required*"
-                    startAdornment={
-                        <InputAdornment position="start">
-                            <AccountCircle />
-                        </InputAdornment>
-                    }
-                    
-                    />
-            </Box>
-                
+            <FormControl required>
+                <InputLabel >Keyword:</InputLabel>
+                    <InputM
+                        id="keyword"
+                        onChange={e=>keywordValidate(e)}
+                        value={keyword}
+                        error={error}
+                        multiline
+                        helperText="press 'return' for multiple keyword search"
+                        label="Required*"
+                        startAdornment={
+                            <InputAdornment position="start">
+                                <AccountCircle />
+                            </InputAdornment>
+                        }
+                        
+                        />
+                        <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
             </FormControl>
+            </Box>
+            <FormGroup className={classes.options}>
+            {/* <FormLabel component="legend">Data to obtain</FormLabel> */}
+                <FormControlLabel
+                    className={classes.label}
+                    disabled
+                    control={ <Checkbox checked={true} name="posts" />}
+                    label="Posts Detail"
+                />
+                <FormControlLabel
+                    className={classes.label}
+                    control={ <Checkbox name="comments" />}
+                    label="Comments"
+                />
+            </FormGroup>
+            <Box display='flex' flexDirection="row" alignItems="center" >
+
+                <Button>Submit</Button>
+            </Box>
+            </React.Fragment>
     )
 }
 
